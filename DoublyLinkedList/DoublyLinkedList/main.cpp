ad0054328214ad6351f45fdf42d07b6afb1084de
Ciri <46222360+LorenzoCirillo@users.noreply.github.com>
2020-09-06 20:59:19 +0200

    Added Sample code to main.cpp #1.1

--- DoublyLinkedList/DoublyLinkedList/main.cpp
+++ DoublyLinkedList/DoublyLinkedList/main.cpp
@@ -3,6 +3,15 @@
 
 int main()
 {
-
+	// Sample code
+	
+	DoublyLinkedList<int> dllint;
+	dllint.PushBack(3);
+	dllint.PushFront(1);
+	dllint.PopBack();
+	
+	for(auto& elem : dllint)
+		std::cout << elem << std::endl;
+	
 	return 0;
 }
